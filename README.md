934df6e1c70c0047c5ec982d2ad43d5c3d7b7be2
pandora2j <casterrow@163.com>
2015-10-21 00:12:49 +0800

    添加国际化配置

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# destto
 Just for study only!
-JDK1.8, Spring, MyBatis
+Spring, MyBatis, MySQL, MyCat, Nginx, Keepalived, Tomcat, CDN, Redis.
+JDK 1.8
+MySQL 5.7
+MyCat 1.4
+Tomcat 8.0.26
+Nginx 1.8.0
+Keepalived 1.2.19 
+Redis 3.0.3
+
--- destto-core/src/main/java/com/github/destto/BaseUtil.java
+++ destto-core/src/main/java/com/github/destto/BaseUtil.java
@@ -2,7 +2,9 @@ package com.github.destto;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 /**
  * 
@@ -11,8 +13,10 @@ import java.util.Properties;
  * @version 1.0
  */
 public class BaseUtil {
-	
+	/**配置文件后缀**/
 	private static final String PROPERTIES = ".properties";
+	/**国际化配置前缀，lang_**.properties**/
+	private static final String i18n = "lang";
 
 	/**
 	 * 加载配置文件
@@ -33,5 +37,17 @@ public class BaseUtil {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	/**
+	 * 加载国际化配置文件，提供简体中文，繁体中文和英文。
+	 * @author casterrow@163.com, Stephen Lau
+	 * @since Oct 21, 2015
+	 * @version 1.0
+	 * @return ResourceBundle
+	 */
+	public static ResourceBundle loadI18n() {
+		ResourceBundle resource = ResourceBundle.getBundle(i18n, Locale.getDefault());
+		return resource == null ? ResourceBundle.getBundle(i18n, Locale.US) : resource;
 	}
 }
