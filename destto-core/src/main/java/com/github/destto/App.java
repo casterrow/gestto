3c97ce443fcc86b6493fa78c164252cef32a3325
pandora2j <casterrow@163.com>
2015-10-20 21:44:10 +0800

    代码提交

--- destto-core/src/main/java/com/github/destto/App.java
+++ destto-core/src/main/java/com/github/destto/App.java
@@ -4,11 +4,6 @@ package com.github.destto;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
 }
